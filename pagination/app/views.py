5895759377ff4f2df8c2347dd38357a20393d357
Utushka <deltacentr24@gmai.com>
2020-07-31 18:33:45 +0300

    исправления

--- pagination/app/views.py
+++ pagination/app/views.py
@@ -12,23 +12,29 @@ def index(request):
 
 
 def bus_stations(request):
-    CONTENT = []
     with open(settings.BUS_STATION_CSV, encoding='cp1251') as csvfile:
         CONTENT = list(csv.DictReader(csvfile))
 
-    by_page = 10
+    by_page = 100
     current_page = int(request.GET.get('page', 1))
     paginator = Paginator(CONTENT, by_page)
-    page_index = paginator.get_paГe(current_page)
+    page_index = paginator.get_page(current_page)
     content = page_index.object_list
-    next_page_url = page_index.next_page_number()
+    if page_index.has_next():
+        next_page_url = page_index.next_page_number()
+    else:
+        next_page_url = current_page
+    if page_index.has_previous():
+        previous_page_url = page_index.previous_page_number()
+    else:
+        previous_page_url = current_page
 
     print(content)
 
     return render_to_response('index.html', context={
         'bus_stations': content,
         'current_page': current_page,
-        'prev_page_url': None,
+        'prev_page_url': f'bus_stations?page={previous_page_url}',
         'next_page_url': f'bus_stations?page={next_page_url}',
     })
 
